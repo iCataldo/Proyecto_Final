e4ae91e3f46abaaafd5adc023e93a9d20dc27fac
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-25 01:57:29 -0300

    Update TablaDirecion.java
    tabla provisional de una matriz simétrica con valores distinto con la distancia de los buses

--- src/TablaDirecion.java
+++ src/TablaDirecion.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
 
 public class TablaDirecion {
-    private int[][] ArrayList=new int[5][5];
+    private int[][] tabla=new int[5][5];
     public TablaDirecion(){
         for (int i=0;i<5;i++){
-            for (int i=0;i<5;i++){
-
+            for (int j=0;j<5;j++){
+                tabla[i][j]=(i+j)*20;
+                if (i==j){
+                    tabla[i][j]=0;
+                }
             }
         }
+    }
+
+    public int getTabla(int x,int y) {
+        return tabla[x][y];
     }
 }
