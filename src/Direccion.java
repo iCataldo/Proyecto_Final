e8da99a0f797607f4e417a6d390d1b625c94c9cf
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-25 02:02:05 -0300

    Update Direccion.java
    dirección: al ingresar una coordenada saca la distancia de la tabla y calculo provisional  del tiempo con la distancia.
    +get distancia devuelve distancia
    + get tiempo devuelve tiempo

--- src/Direccion.java
+++ src/Direccion.java
@@ -1,5 +1,16 @@
 public class Direccion {
+    private int direcion;
+    private int tiempo;
     public Direccion(int x,int y){
+        TablaDirecion tabla =new TablaDirecion();
+        direcion=tabla.getTabla(x, y);
+        tiempo=tabla.getTabla(x, y)/2;
+    }
+    public int getDirecion() {
+        return direcion;
+    }
 
+    public int getTiempo() {
+        return tiempo;
     }
 }
