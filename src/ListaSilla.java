5ffce7a4280402d977343295f4d1a00f41efee19
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-27 22:15:44 -0300

    Update ListaSilla.java

--- src/ListaSilla.java
+++ src/ListaSilla.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class ListaSilla {
-    private ArrayList<Silla> ver;
     private Silla[][] tabla;
     private int CordenadaX=0;
     private int CordenadaY=0;
     public ListaSilla(int n){
-        tabla= new Silla[4][n/4+1];
+        tabla= new Silla[n/4+1][4];
     }
     public void addSilla(Silla silla){
 
-        tabla[CordenadaX][CordenadaY]=silla;
+        tabla[CordenadaY][CordenadaX]=silla;
+        silla.setXY(CordenadaX,CordenadaY);
         CordenadaX++;
         if (CordenadaX==4){
             CordenadaX=0;
@@ -20,8 +20,8 @@ public class ListaSilla {
     public void printf(){
         for (int i=0;i<=CordenadaY;i++){
             for (int j=0;j<4;j++){
-                if (tabla[j][i]!=null) {
-                    System.out.println(i + "," + j + ": " + tabla[j][i].precio);
+                if (tabla[i][j]!=null) {
+                    System.out.println((i+1)+ "," + (j+1)+ ": " + tabla[i][j]);
                 }
             }
 
