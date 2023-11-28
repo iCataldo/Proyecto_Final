e8fec877aae059977a2f6c7b37382221f96566f5
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-28 00:00:40 -0300

    Update Buses.java
    buses constructor usa los métodos de listado para agregar distintos tipo de sillas

--- src/Buses.java
+++ src/Buses.java
@@ -1,5 +1,21 @@
 public class Buses {
-    public Buses(){
+    private ListaSilla list;
+    private int capacidad;
+    public Buses(Direccion direc,int Spref,int Snorm,int Scama){
+        capacidad=Snorm + Spref + Scama;
+        list = new ListaSilla(capacidad);
+        for (int i=0;i<Spref;i++){
+            list.addSilla(new SillaPreferencial());
+        }
+        for (int i=0;i<Snorm;i++){
+            list.addSilla(new SillaNormal());
+        }
+        for (int i=0;i<Scama;i++){
+            list.addSilla(new SillaCama());
+        }
+    }
 
+    public void print() {
+        list.printf();
     }
 }
